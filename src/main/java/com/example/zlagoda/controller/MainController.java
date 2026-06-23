dc043fcd94fd6f2e628579d230a9e607fcfbbddf
int3rrobang <80174663+int3rrobang@users.noreply.github.com>
2026-06-23 12:06:14 +0300

    Polish login and required field UI

--- src/main/java/com/example/zlagoda/controller/MainController.java
+++ src/main/java/com/example/zlagoda/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.example.zlagoda.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,7 +13,12 @@ public class MainController {
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Authentication authentication) {
+        if (authentication != null
+                && authentication.isAuthenticated()
+                && !(authentication instanceof AnonymousAuthenticationToken)) {
+            return "redirect:/";
+        }
         return "login";
     }
 }
